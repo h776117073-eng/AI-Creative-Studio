e11c8ca4ea9e5b3450c6d89de7d082a20320e702
h776117073-eng <h776117073@gmail.com>
2026-09-01 23:22:49 +0300

    feat(core): expose render graph APIs

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,4 +4,5 @@ export * from './events/index.js';
 export * from './engines/index.js';
 export * from './state/index.js';
 export * from './utils/index.js';
+export * from './render/index.js';
 export { EventEmitter } from 'eventemitter3';
